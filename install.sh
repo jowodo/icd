aeece1b78304fb36ddcaeba765c85460e9858e9b
pur80a <j.w.dorn@gmx.at>
2020-12-18 14:44:10 +0100

    /dev/null added to install

--- install.sh
+++ install.sh
@@ -19,18 +19,25 @@ PWD=$(pwd)
 mkdir -p $HOME/.icd
 cp -v $PWD/icd $HOME/.icd/icd
 
-echo "=============================================="
+###count length of destdir
+lod=$(echo -n $DESTDIR | wc -c )
+
+head -c $lod /dev/zero | tr '\0' '='
+echo "======================================"
 echo "| sourcing $DESTDIR/icd from following file: |"
-echo "=============================================="
+head -c $lod /dev/zero | tr '\0' '='
+echo "======================================"
 echo "[1] ~/.bashrc (default)"
 echo "[2] ~/.bash_functions"
 echo "[3] other file"
+echo "[4] /dev/null"
 read input 
 if [[ $input = 2 ]]; then DESTFILE=$HOME/.bash_functions ; fi
 if [[ $input = 3 ]]; then
 	echo -n "Please specify location: "
 	read DESTFILE
 fi
+if [[ $input = 4 ]]; then DESTFILE=/dev/null ; fi 
 
 echo "following text was apended to $DESTFILE"
 echo "if [ -f $HOME/.icd/icd ] ; then source $HOME/.icd/icd ; fi" | tee -a $DESTFILE
